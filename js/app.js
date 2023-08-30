e2e664303fa3363e6f08e6a11504719939fa0db4
eman eldeeb <emaneldeeb1999@gmail.com>
2023-08-31 01:36:04 +0200

    add countdown

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /// <reference types="../@types/jquery" />
 
+// sideNav
 $(".side_control").on("click", function () {
   $(".side-menu").css({ left: "0" });
 });
@@ -13,6 +14,21 @@ $(".singers h3").on("click", function (e) {
   $(".singers h3").not(this).next().slideUp();
   $(this).next().slideToggle();
 });
+
+// date count down
+const countDownDate = new Date("Nov 19,2023 22:22:22").getTime();
+let counter = setInterval(() => {
+  let timeNow = new Date().getTime();
+  let dataDiff = countDownDate - timeNow;
+  $(".day span").text(`${Math.floor(dataDiff / 1000 / 60 / 60 / 24)} D`);
+  $(".hour span").text(
+    `${Math.floor((dataDiff % (1000 * 60 * 60 * 24)) / 1000 / 60 / 60)} H`
+  );
+  $(".min span").text(
+    `${Math.floor((dataDiff % (1000 * 60 * 60)) / 1000 / 60)} M`
+  );
+  $(".sec span").text(`${Math.floor((dataDiff % (1000 * 60)) / 1000)} s`);
+}, 1000);
 
 // count
 $("#message").on("input", function () {
