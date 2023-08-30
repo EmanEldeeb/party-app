084249317234ac8293bf336740fa9b5bb2740a7e
eman eldeeb <emaneldeeb1999@gmail.com>
2023-08-31 01:12:04 +0200

    add functionlaty

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 /// <reference types="../@types/jquery" />
+
+$(".side_control").on("click", function () {
+  $(".side-menu").css({ left: "0" });
+});
+
+$(".move .close").on("click", function () {
+  $(".side-menu").css({ left: "-300px" });
+});
+
+// singers
+$(".singers h3").on("click", function (e) {
+  $(".singers h3").not(this).next().slideUp();
+  $(this).next().slideToggle();
+});
+
+// count
+$("#message").on("input", function () {
+  let textLen = $(this).val().length;
+  $(".charcount").text(100 - textLen <= 0 ? "out of char" : 100 - textLen);
+});
